622058308e348331c3e1c6ef7a3f499889c1a4e1
Cristoval Pratama Siahaan <pratamacristoval@gmail.com>
2025-11-30 00:52:21 +0700

    pelan pelan

--- api/routes/categoryRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const categoryController = require("../controllers/categoryController");
-
-// GET /api/categories
-router.get("/categories", categoryController.getAllCategories);
-
-module.exports = router;
